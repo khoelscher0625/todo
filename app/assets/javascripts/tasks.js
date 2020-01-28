8a5596da32207a4dbbfdc76988c1acfb0dae1117
Kathryn Hoelscher <khoelscher0625@gmail.com>
2020-01-28 01:55:33 +0000

    added the todomvc design package to the app

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -59,6 +59,7 @@ $(function() {
         var ulTodos = $('.todo-list');
         ulTodos.append(htmlString);
         $('.toggle').click(toggleTask);
+        $('.new-todo').val('');
       });
     });
   });
